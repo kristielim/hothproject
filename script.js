0cde77e4f0c04fb192a142b32e8861a6072fafff
unknown <kristie.lim@gmail.com>
2017-11-12 15:50:36 -0800

    adds activities from csv file

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function handleImageClick(event) {
 		if(tag != "any")
 			userTags.push(tag);
 	}
-	const activityTags = ['explore', 'day'];
+	console.log(userTags);
+	const activityTags = ['explore'];
 	if(isGoodActivity(userTags,activityTags)){
 		alert("Good activity");
 	}
@@ -22,24 +23,52 @@ function handleImageClick(event) {
 $('document').ready(function() {
 
 	attachEventHandlers();
+	$.ajax({
+        type: "GET",
+        url: "data:application/vnd.ms-excel;base64,QWN0aXZpdHksRWF0L0V4cGxvcmUsVGltZSxUcmFuc3BvcnQsQnVkZ2V0DQpEaWRkeSBSaWVzZSxFYXQgLGxlc3MgdGhhbiBhbiBob3VyLHdhbGssJA0KRm94IFRoZWF0ZXIsRXhwbG9yZSxhIGZldyBob3Vycyx3YWxrLCQkDQpIYW1tZXIgTXVzZXVtLEV4cGxvcmUsZGF5LHdhbGsgLEZSRUUNCkZvd2xlciBNdXNldW0gLEV4cGxvcmUsZGF5LFdhbGssRlJFRQ0KTSBFIE0gQm90YW5pY2FsIEdhcmRlbixFeHBsb3JlLGRheSxXYWxrLEZSRUUNClNhbnRhIE1vbmljYSBQaWVyLEV4cGxvcmUsZGF5ICxCdXMsJA0KR2V0dHkgQ2VudGVyLEV4cGxvcmUsZGF5LEJpa2UsRlJFRQ0KU2N1bHB0dXJlIEdhcmRlbixFeHBsb3JlLGxlc3MgdGhhbiBhbiBob3VyLFdhbGssRlJFRQ0KRmF0IFNhbCdzICxFYXQsbGVzcyB0aGFuIGFuIGhvdXIsd2FsaywkDQpEaXNuZXlsYW5kLEV4cGxvcmUsZGF5LEJ1cywkJCQNCg==",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
 
 });
 
 function isGoodActivity(userTags, activityTags){
-	var count = 0;
+	let count = 0;
 
-for(var i = 0; i < userTags.length; i++){
-	for(var k = 0; k < activityTags.length; k++){
-		if(userTags[i].toLowerCase() == activityTags[k].toLowerCase()){
-			count++;
+	for(let i = 0; i < userTags.length; i++){
+		for(let k = 0; k < activityTags.length; k++){
+			if(userTags[i].toLowerCase() == activityTags[k].toLowerCase()){
+				count++;
+			}
 		}
 	}
-}
 
 	if(count == userTags.length) {
-	return true;
-}
-else{
-return false;
+		return true;
 	}
+	else {
+		return false;
+	}
+}
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            tarr.push(data[0]);
+            var tags = [];
+            for (var j=1; j<headers.length; j++) {
+                tags.push(data[j]);
+            }
+            tarr.push(tags);
+            lines.push(tarr);
+        }
+    }
+    console.log(lines[0]);
 }
