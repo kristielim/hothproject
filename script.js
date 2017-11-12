ed43cd0e84424c376a0450a9bc3ee8827927ed99
Michelle Tan <michellektan>
2017-11-12 14:50:54 -0800

    check for good activity

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function handleImageClick(event) {
 		if(tag != "any")
 			userTags.push(tag);
 	}
-	console.log(userTags);
+	const activityTags = ['explore', 'day'];
+	if(isGoodActivity(userTags,activityTags)){
+		alert("Good activity");
+	}
+
 }
 
 $('document').ready(function() {
@@ -20,3 +24,22 @@ $('document').ready(function() {
 	attachEventHandlers();
 
 });
+
+function isGoodActivity(userTags, activityTags){
+	var count = 0;
+
+for(var i = 0; i < userTags.length; i++){
+	for(var k = 0; k < activityTags.length; k++){
+		if(userTags[i].toLowerCase() == activityTags[k].toLowerCase()){
+			count++;
+		}
+	}
+}
+
+	if(count == userTags.length) {
+	return true;
+}
+else{
+return false;
+	}
+}
